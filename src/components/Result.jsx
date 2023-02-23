b4f76891273f7d71db3f694b4895c52104cbc179
Ayo Moses <ayomoses@afrimash.com>
2023-02-23 14:13:19 +0100

    Integrated calculator functionality

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
 
-function Result() {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Result({ total, next, operation }) {
   return (
-    <div className="result">0</div>
+    <div className="result">
+      {total}
+      {' '}
+      {operation}
+      {' '}
+      {next}
+    </div>
   );
 }
+
+Result.propTypes = {
+  total: PropTypes.number.isRequired,
+  next: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
+};
 
 export default Result;
